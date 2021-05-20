9f943e4715dc89069db4635b0db12f025f23d3c7
novanabs <novanabs@gmail.com>
2021-05-20 22:14:32 +0800

    tambah softdelete terhadap meeting

--- app/Meeting.php
+++ app/Meeting.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Meeting extends Model
 {
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
